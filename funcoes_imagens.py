d476c6a665571f7ebab1a5eb32c9c900a07fe70a
ViniNovack <vininomn@gmail.com>
2026-05-30 19:17:58 -0300

    Atualização

--- funcoes_imagens.py
+++ funcoes_imagens.py
@@ -1,4 +1,6 @@
 import shutil
+import time
+
 
 def titulo():
     titulo = ("""                                                                                                                                                                                                                            
@@ -88,3 +90,109 @@ def episodio():
         print(linha_centralizada)
 
 
+
+def texto():
+    texto = """
+    É um tempo de tirania sombria. Por décadas, o pacífico povo Gungan foi subjugado pela opressão
+    implacável do IMPÉRIO GALÁCTICO. Encorajados pela coragem e liderança da PRINCESA LEIA
+    ORGANA, os outrora reclusos Gungans finalmente se levantaram em uma rebelião desesperada,
+    clamando por liberdade em seu próprio mundo natal.
+
+    Nas profundezas dos oceanos de Naboo, as outrora brilhantes cidades submarinas
+    agora jazem na escuridão. O grito de revolta ecoou pelos pântanos e planícies,
+    unindo os clãs Gungans sob uma única bandeira de resistência, jurando expulsar as
+    guarnições imperiais que profanam suas terras sagradas.
+
+    A retaliação imperial, porém, foi imediata e cruel. Sob as ordens de oficiais
+    implacáveis, o Império estabeleceu um bloqueio orbital absoluto, cortando todo o
+    fornecimento de água potável e mantimentos para a superfície e para as
+    profundezas. A fome e a sede assolam a população, ameaçando extinguir a cultura
+    Gungan antes mesmo que a verdadeira guerra comece.
+
+    Enquanto o desespero consome o planeta, a resistência clandestina luta contra o
+    relógio para manter acesa a última chama de esperança. Mensagens codificadas
+    são enviadas para os confins da galáxia, mas poucos ousam desafiar a frota de
+    Destroieres Estelares que vigia os céus de Naboo como predadores famintos.
+
+    Em uma missão de pura misericórdia e alto risco, a audaciosa EQUIPE DELTA foi
+    designada pela Princesa Leia para romper o cerco. Transportando toneladas de
+    suprimentos vitais em um cargueiro corelliano camuflado, o grupo de rebeldes
+    traçou uma rota perigosa, confiando cegamente nas coordenadas de infiltração
+    fornecidas por um informante local.
+
+    Mas o destino reservava uma armadilha terrível. Uma traição vil nos altos escalões
+    da resistência entregou os planos exatos da operação aos comandantes imperiais.
+    Ao chegarem na ESTAÇÃO DE POUSO 4, os heróis foram recebidos pelo fogo
+    cruzado de uma guarnição inteira de Stormtroopers, transformando a missão de
+    resgate em um pesadelo de fumaça e lasers.
+
+    Em meio ao caos do combate e demonstrando uma bravura inacreditável, a Equipe
+    Delta conseguiu descarregar os mantimentos nos dutos de ventilação que levavam
+    direto às colônias Gungans, garantindo a sobrevivência temporária dos inocentes.
+    Mas o preço do sucesso foi alto: os escudos da nave rebelde foram desintegrados e
+    o hiperpropulsor começou a falhar.
+
+    Agora, com os alarmes de emergência ecoando pela cabine e caças TIE caçando
+    sua assinatura de calor na atmosfera superior, os heróis da Equipe Delta precisam
+    realizar uma fuga impossível. Sem apoio e cercados pelas forças inimigas...
+    """
+    largura_terminal = shutil.get_terminal_size().columns
+    for linha in texto.split('\n'):
+        # Centraliza a linha de acordo com o tamanho do terminal
+        linha_centralizada = linha.center(largura_terminal)
+        print(linha_centralizada)
+        time.sleep(1.5)
+
+
+
+def centr(texto):
+    largura_terminal = shutil.get_terminal_size().columns
+    linha_centralizada = texto.center(largura_terminal)
+    print(linha_centralizada)
+
+
+
+def imperio():
+    imperio = """
+    MMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxdocc::;;;;;::cldxO0XWMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMMWXOdc;..                    ..,cdOXWMMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMNOo;.      ..,;clo;   .:olc:,..      .;oONMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMNkc'     .;ldk0xolc:;.    ';:clok0Odl;.     'lONMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMWKo,    .;okXWMMMNc                cNMMMWXko,.    ,dXWMMMMMMMMMMMMM
+    MMMMMMMMMMMW0c.   .;d0WMMMMWX0x;                ;x0XWMMMMW0o,    .lKWMMMMMMMMMMM
+    MMMMMMMMMWKc.   .lONMMMMNOo:..                     .;oONWMMMNOc.   .oXMMMMMMMMMM
+    MMMMMMMMNd.   .lk0NMMNOl'        .;cloooooolc,.        'lONMMXOkc.   'xNMMMMMMMM
+    MMMMMMW0;   .ckd'.,dd,           ,KMMMMMMMMMMO.           ,do'.,xk;    cKMMMMMMM
+    MMMMMWx.   'xk,                   oWMMMMMMMMNc                   :Od.   ,OWMMMMM
+    MMMMNo.    ,:.        ..          '0MMMMMMMWx.          .         .c'    .xWMMMM
+    MMMNl   .,.         .:OOc.         lNMMMMMMK;         'd0x,          ''   .xWMMM
+    MMWo.  .o0,        ,kWMMW0c.       .kMMMMMWd        'dXMMMXo.        c0c   .kWMM
+    MWk.   l0;        :KMMMMMWW0:.    ':OWMMMMNx;.    .oXMMMMMMWk.        l0:   '0MM
+    MK;   ;Kx.       cXMMMMMMMMMWO:,lONWMMMMMMMMWXkc,oXMMMMMMMMMWO'      .,O0'   cNM
+    Wd.  .xWNKO:    .kNWMMMMMMMMMMWWWMMMMMMMMMMMMMMWWWMMMMMMMMMWN0c     c0XWWd   .kM
+    N:   ;KMWMNc     ..,:ldk0XWMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxoc;..      lNMWMK,   lW
+    0'   lWMMM0'            ..,lKMMMMMMMMMMMMMMMMMMMMMM0:'.             ,KMMMWc   ;X
+    k.  .xMMMMk.               ,KMMMMMMMMMMMMMMMMMMMMMMO.               .OMMMMo   '0
+    x.  .xMMMMk.               ,KMMMMMMMMMMMMMMMMMMMMMMO.               .OMWMMd   .O
+    k.  .xMMMMO.            .';oXMMMMMMMMMMMMMMMMMMMMMMKl;'.            ,KMMMWo   '0
+    0'   lWMMMX;     .';coxOKNWMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxoc;'.     cNMWMN:   ;X
+    N:   ,KMWN0:    .kNWMMMMMMMMMMNNWMMMMMMMMMMMMMMNXWMMMMMMMMMMWNx.    cKNWMO.   oW
+    Mx.   dNo..      cXMMMMMMMMMNx,.ckKWMMMMMMMMN0d;.:OWMMMMMMMMMK;      .'xNc   '0M
+    MX:   .Ox.        cXMMMMMMXx,     .:0MMMMMWk,.    .:OWMMMMMW0;        'Od.   oWM
+    MM0'   ,0d.        ;0WMMXd'        ;KMMMMMMO.       .:OWWWNx.        .kk.   :XMM
+    MMWk.   ,l.         .oOd'         .kWMMMMMMWl         .:kk:          'c.   ;KMMM
+    MMMWk.     ..                     cNMMMMMMMM0'                     '.     ;KMMMM
+    MMMMWO'    cOl.                  '0MMMMMMMMMWd.                  .dO:    :KMMMMM
+    MMMMMMK:    ,xk;..oko'           lNMMMMMMMMMW0,           ,xOl..cOx'   .dNMMMMMM
+    MMMMMMMNx'   .:kOKWMMXk:.        .,:ccllllc:;'.        .cONMMWKOx,    ;OWMMMMMMM
+    MMMMMMMMMKl.   .;kXMMMMWXkc,.                      .,lkXWMMMWXx,    'xNMMMMMMMMM
+    MMMMMMMMMMWKl.    'o0NMMMMMNKko'                ,okKWMMMMWXkc.    'dXMMMMMMMMMMM
+    MMMMMMMMMMMMWKo'    .,lkKWMMMMX:                cNMMMMNKxc'    .;xXMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMNkc'     .;lxOKKxc:;,'.    .',;:cxKKkdc,.    .,o0WMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMWNkl,.      .';:lood:   .cdool:;'.      .;o0NMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMWKxl;'.                        .':okXWMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMMMMMWNKkdoc:,'.........'';:loxOKWMMMMMMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXXKKKXXXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+    """
+    centr(imperio)
--- main.py
+++ main.py
@@ -4,78 +4,32 @@ import os
 import pygame
 import funcoes_imagens
 
+ff = funcoes_imagens
+
 def main():
     pygame.mixer.init()
     pygame.mixer.music.load('Star-Wars-Main-Theme-_Full_.ogg') 
     pygame.mixer.music.play()
     
+    ff.episodio()
     time.sleep(2)
-    funcoes_imagens.episodio()
+    os.system('cls')
 
     time.sleep(2)
-    funcoes_imagens.titulo()
-    
+    ff.titulo()
+       
     time.sleep(1)
+    #ff.texto()
+
+    # for n in range(0, 36):
+    #     time.sleep(1)
+    #     print()
+
     os.system('cls')
-    texto_abertura = f"""
-    É um tempo de tirania sombria. Por décadas, o pacífico povo Gungan foi subjugado pela opressão
-    implacável do IMPÉRIO GALÁCTICO. Encorajados pela coragem e liderança da PRINCESA LEIA
-    ORGANA, os outrora reclusos Gungans finalmente se levantaram em uma rebelião desesperada,
-    clamando por liberdade em seu próprio mundo natal.
+    ff.centr('AGORA ESCOLHA UM LADO E DECIDA O DESTINO DESSA HISTÓRIA')
+    time.sleep(1)
 
-    Nas profundezas dos oceanos de Naboo, as outrora brilhantes cidades submarinas
-    agora jazem na escuridão. O grito de revolta ecoou pelos pântanos e planícies,
-    unindo os clãs Gungans sob uma única bandeira de resistência, jurando expulsar as
-    guarnições imperiais que profanam suas terras sagradas.
 
-    A retaliação imperial, porém, foi imediata e cruel. Sob as ordens de oficiais
-    implacáveis, o Império estabeleceu um bloqueio orbital absoluto, cortando todo o
-    fornecimento de água potável e mantimentos para a superfície e para as
-    profundezas. A fome e a sede assolam a população, ameaçando extinguir a cultura
-    Gungan antes mesmo que a verdadeira guerra comece.
-
-    Enquanto o desespero consome o planeta, a resistência clandestina luta contra o
-    relógio para manter acesa a última chama de esperança. Mensagens codificadas
-    são enviadas para os confins da galáxia, mas poucos ousam desafiar a frota de
-    Destroieres Estelares que vigia os céus de Naboo como predadores famintos.
-
-    Em uma missão de pura misericórdia e alto risco, a audaciosa EQUIPE DELTA foi
-    designada pela Princesa Leia para romper o cerco. Transportando toneladas de
-    suprimentos vitais em um cargueiro corelliano camuflado, o grupo de rebeldes
-    traçou uma rota perigosa, confiando cegamente nas coordenadas de infiltração
-    fornecidas por um informante local.
-
-    Mas o destino reservava uma armadilha terrível. Uma traição vil nos altos escalões
-    da resistência entregou os planos exatos da operação aos comandantes imperiais.
-    Ao chegarem na ESTAÇÃO DE POUSO 4, os heróis foram recebidos pelo fogo
-    cruzado de uma guarnição inteira de Stormtroopers, transformando a missão de
-    resgate em um pesadelo de fumaça e lasers.
-
-    Em meio ao caos do combate e demonstrando uma bravura inacreditável, a Equipe
-    Delta conseguiu descarregar os mantimentos nos dutos de ventilação que levavam
-    direto às colônias Gungans, garantindo a sobrevivência temporária dos inocentes.
-    Mas o preço do sucesso foi alto: os escudos da nave rebelde foram desintegrados e
-    o hiperpropulsor começou a falhar.
-
-    Agora, com os alarmes de emergência ecoando pela cabine e caças TIE caçando
-    sua assinatura de calor na atmosfera superior, os heróis da Equipe Delta precisam
-    realizar uma fuga impossível. Sem apoio e cercados pelas forças inimigas...
-    """
-
-    # Largura atual do terminal do VS Code
-    largura_terminal = shutil.get_terminal_size().columns
-    # Empurra o texto para baixo inicialmente
-    print("\n" * 15)
-    for linha in texto_abertura.split('\n'):
-        # Centraliza a linha de acordo com o tamanho do terminal
-        linha_centralizada = linha.center(largura_terminal)
-        print(linha_centralizada)
-        time.sleep(2)
-    
-    for n in range(0, 36):
-        time.sleep(1)
-        print()
-    os.system('cls')
 
 if __name__ == '__main__':
     main()
